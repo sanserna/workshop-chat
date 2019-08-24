7afe867c073cb621d1ef751fbb7bc4ed50d46888
Santiago Serna <sanserve91@gmail.com>
2019-08-24 01:58:00 -0500

    update chat body to load messages from server

--- src/components/Chat/ChatBody/ChatBody.jsx
+++ src/components/Chat/ChatBody/ChatBody.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
+import { Store } from 'Store';
 import styles from './ChatBody.module.scss';
 import Message from './Message';
 
+const GET_CHATS = gql`
+  query ChatsQuery {
+    chats {
+      id
+      from
+      fromName
+      message
+    }
+  }
+`;
+
 function ChatBody() {
+  const { state } = useContext(Store);
   const chatBodyClassNames = [styles['chat-body'], 'elevation-box-inset-right'];
+  const { data, loading } = useQuery(GET_CHATS);
+  const chatList = data.chats;
 
   return (
     <section className={chatBodyClassNames.join(' ')}>
-      <Message fromName="Santiago" message="Hola" isOwn />
+      {loading
+        ? 'Cargando mensajes...'
+        : chatList.map(chat => (
+            <Message
+              key={chat.id}
+              fromName={chat.fromName}
+              message={chat.message}
+              isOwn={chat.from === state.user.id}
+            />
+          ))}
     </section>
   );
 }
