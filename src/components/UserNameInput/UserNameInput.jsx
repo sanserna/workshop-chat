5c2a113e5fef6b187d7342cb5c3e31f512ed371d
Santiago Serna <sanserve91@gmail.com>
2019-08-24 01:45:17 -0500

    update user name input component

--- src/components/UserNameInput/UserNameInput.jsx
+++ src/components/UserNameInput/UserNameInput.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import Input from 'components/base/Input';
 import Button from 'components/base/Button';
+import { Store } from 'Store';
 import styles from './UserNameInput.module.scss';
 
+const ID = () => {
+  return `_${Math.random()
+    .toString(36)
+    .substr(2, 9)}`;
+};
+
 function UserNameInput() {
+  const { dispatch } = React.useContext(Store);
   const [userName, setUserName] = React.useState('');
 
   return (
@@ -15,7 +23,15 @@ function UserNameInput() {
         value={userName}
         onChange={e => setUserName(e.target.value)}
       />
-      <Button disabled={!userName} onClick={() => {}}>
+      <Button
+        disabled={!userName}
+        onClick={() => {
+          dispatch({
+            type: 'SET_USER',
+            payload: { id: ID(), name: userName }
+          });
+        }}
+      >
         Go
       </Button>
     </div>
