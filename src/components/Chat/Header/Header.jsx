fba25402d3ef52be4043222ae5d947a0c11579ff
Santiago Serna <sanserve91@gmail.com>
2019-08-24 01:47:02 -0500

    update chat header component

--- src/components/Chat/Header/Header.jsx
+++ src/components/Chat/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { Store } from 'Store';
 import styles from './Header.module.scss';
 
 function Header() {
+  const { state } = React.useContext(Store);
+
   return (
     <header className={styles.header}>
-      <h2 className={styles.header__title}>Hola Santiago</h2>
+      <h2 className={styles.header__title}>Hola {state.user.name}</h2>
       <img src="assets/zemoga-logo.svg" alt="Zemoga" />
     </header>
   );
