fff8fcd9c212b2f7b524c11638e8b3fe8a7233c8
Santiago Serna <sanserve91@gmail.com>
2019-08-24 01:44:00 -0500

    update app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import Chat from 'components/Chat';
+import UserNameInput from 'components/UserNameInput';
+import { Store } from './Store';
 import styles from './App.module.scss';
 
 function App() {
+  const { state } = React.useContext(Store);
+  const userExists = !!Object.keys(state.user).length;
+
   return (
     <div className={styles['app-container']}>
-      <Chat />
+      {userExists ? <Chat /> : <UserNameInput />}
     </div>
   );
 }
