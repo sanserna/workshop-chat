672d8085b9043f9f96bb4d81cdca0cd7f5b55b56
Santiago Serna <sanserve91@gmail.com>
2019-08-24 02:02:34 -0500

    update message input to use send messages mutation

--- src/components/Chat/MessageInput/MessageInput.jsx
+++ src/components/Chat/MessageInput/MessageInput.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
+import { Store } from 'Store';
 import Input from 'components/base/Input';
 import Button from 'components/base/Button';
 import styles from './MessageInput.module.scss';
 
+const SEND_MESSAGE_MUTATION = gql`
+  mutation SendMessage($from: ID!, $fromName: String!, $message: String!) {
+    sendMessage(from: $from, fromName: $fromName, message: $message) {
+      id
+      from
+      fromName
+      message
+    }
+  }
+`;
+
 function MessageInput() {
+  const { state } = useContext(Store);
+  const [sendMessage] = useMutation(SEND_MESSAGE_MUTATION);
   const [message, setMessage] = React.useState('');
 
   return (
@@ -14,7 +30,23 @@ function MessageInput() {
         value={message}
         onChange={e => setMessage(e.target.value)}
       />
-      <Button onClick={() => {}}>Enviar</Button>
+      <Button
+        onClick={() => {
+          if (!message) return;
+
+          sendMessage({
+            variables: {
+              message,
+              from: state.user.id,
+              fromName: state.user.name
+            }
+          }).then(() => {
+            setMessage('');
+          });
+        }}
+      >
+        Enviar
+      </Button>
     </div>
   );
 }
